230290e5d334a08546b75e930770fbf3d3e002af
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-09-18 16:19:45 +0530

    Create 4_LargestSubarrayOfSum0.cpp

---
+++ Strivers/Day4/4_LargestSubarrayOfSum0.cpp
@@ -0,0 +1,26 @@
+// Link: https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+// Using Prefix Sum
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        int i=0, j=0, sum =0;
+        int max_len = 0;
+        unordered_map<int, int> prefix_sum;
+        while(j<n){
+            sum += A[j];
+            
+            if(sum == 0)
+                max_len = j + 1;
+            
+            if(prefix_sum.find(sum) != prefix_sum.end())
+                max_len = max(max_len, j-prefix_sum[sum]);
+            else{
+                prefix_sum[sum] = j;
+            }
+            j++;
+        }
+        
+        return max_len;
+    }
+};
