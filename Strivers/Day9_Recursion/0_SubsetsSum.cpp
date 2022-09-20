f807a96f6a8b7d0937e4f4e548ec69b8ae5fee4f
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-09-20 13:27:47 +0530

    Create 0_SubsetsSum.cpp

---
+++ Strivers/Day9_Recursion/0_SubsetsSum.cpp
@@ -0,0 +1,23 @@
+// Link: https://practice.geeksforgeeks.org/problems/subset-sums2234/1
+
+class Solution
+{
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector<int> subsetSum;
+        int total_subsets = pow(2, N);
+        
+        for(int i=0; i<total_subsets; i++){
+            int sum = 0;
+            for(int j=0; j<N; j++){
+                if((i & (1 << j)) != 0){
+                    sum += arr[j];
+                }
+            }
+            subsetSum.push_back(sum);
+        }
+        
+        return subsetSum;
+    }
+};
