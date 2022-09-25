4b3f738c143d2ce1e1c88fafda0adc937d05adbf
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-09-25 22:17:55 +0530

    Update 5_KthElementInSortedArray.cpp

--- Strivers/Day11/BinarySearch/5_KthElementInSortedArray.cpp
+++ Strivers/Day11/BinarySearch/5_KthElementInSortedArray.cpp
@@ -23,3 +23,41 @@ class Solution{
      return ans;   
     }
 };
+
+/*
+class Solution{
+    public:
+    long long count_element(long long ele, int arr[], int s){
+        long long low = 0;
+        long long high = s-1;
+        
+         while(low <= high){
+             long long mid = (low+high) >> 1;
+             //cout<<"M: "<<mid<<endl;
+             if(arr[mid] <= ele){
+                 low = mid + 1;
+             }
+             else
+                high = mid - 1;
+         }
+         return high+1;         
+    }
+    
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+     long long low = min(arr1[0], arr2[0]);
+     long long high = max(arr1[n-1], arr2[m-1]);
+     while(low <= high){
+         long long mid = (low+high) >> 1;
+         long long cnt = count_element(mid, arr1, n) + count_element(mid, arr2, m);
+         //cout<<"C: "<<cnt<<endl;
+         if(cnt <= k-1){
+             low = mid + 1;
+         }
+         else
+            high = mid - 1;
+     }
+     return low;   
+    }
+};
+*/
