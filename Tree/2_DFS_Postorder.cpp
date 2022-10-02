d74b93d8ad778d220b27bd7a6ad374d34d501ab1
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-10-02 19:56:09 +0530

    Create 2_DFS_Postorder.cpp

---
+++ Tree/2_DFS_Postorder.cpp
@@ -0,0 +1,30 @@
+//Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void postorder(TreeNode* node, vector<int>& res){
+        if(node == NULL)
+            return;
+        postorder(node->left, res);
+        postorder(node->right, res);
+        res.push_back(node->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        
+        postorder(root, res);
+        
+        return res;
+    }
+};
