9534d050f00e944d70752cd947be3b4cd874531e
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-10-02 19:56:42 +0530

    Create 3_BFS.cpp

---
+++ Tree/3_BFS.cpp
@@ -0,0 +1,49 @@
+//Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void bfs(TreeNode* root, vector<vector<int>>& res){
+        // If there are no nodes, simply return.
+        if(root == NULL) return;
+        
+        // Create a queue to store the nodes that needs to be processed in the queue
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            vector<int> level;
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                // Take out the node from the queue
+                TreeNode* node = q.front();
+                q.pop();
+                
+                // Add the left and right elements in the queue, so that they can be preocessed lated in the level order 
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+                
+                level.push_back(node->val);
+            }
+            // Add the level data
+            res.push_back(level);
+        }
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        
+        bfs(root, res);
+        
+        return res;
+    }
+};
