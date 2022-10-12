2f8b4f25cfbfe3819e706bbda863468e0d946606
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-10-12 15:48:52 +0530

    Create 0_BST.cpp

---
+++ Graph/0_BST.cpp
@@ -0,0 +1,70 @@
+//Link: https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> bfs_res;
+        vector<int> vis(V, 0);
+        
+        //for(int i=0; i<V; i++){ //Uncomment to traverse the disconnected componentes as well
+            //if(!vis[i]){
+                
+                //BFS
+                queue<int> q;
+                q.push(0);
+                vis[0] = 1;
+                
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    bfs_res.push_back(node);
+                    
+                    for(auto item : adj[node]){
+                        if(!vis[item]){
+                            q.push(item);
+                            vis[item] = 1;
+                        }
+                    }
+                }
+            //}
+        //}
+        return bfs_res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
