054b3fb49d6f4326a49efa761876c73a7c8a1fc2
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-10-13 10:23:13 +0530

    Update Fundamentals.cpp

--- String/Fundamentals.cpp
+++ String/Fundamentals.cpp
@@ -1,4 +1,19 @@
 /*
+  Conversions
+*/
+int main() {
+	string s = "5";
+	char c = '5';
+	int a = 10;
+	double d = 10.0;
+	cout<<"String to Int: "<<stoi(s)<<endl;
+	cout<<"Char to INT:"<<c - '0'<<endl;			// LOOK OUT
+	cout<<"Int to String:"<<to_string(a)<<endl;
+	cout<<"Double to String:"<<to_string(d)<<endl;
+	return 0;
+}
+
+/*
 How to split a string by a delimiter?
 */
 
