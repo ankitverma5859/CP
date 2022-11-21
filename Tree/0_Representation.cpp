ff8aba4bffbe0997c0ea96cc68ee0036db846336
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-11-21 09:42:53 +0530

    Create 0_Representation.cpp

---
+++ Tree/0_Representation.cpp
@@ -0,0 +1,27 @@
+struct Node{
+  int data;
+  struct Node* left;
+  struct Node* right;
+  
+  Node(int val){
+    data = val;
+    left = right = null;
+  }
+}
+
+
+int main(){
+  struct Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->right = new Node(4);
+}
+
+/*
+        1
+       /  \
+     2      3
+       \
+        4
+*/
+    
