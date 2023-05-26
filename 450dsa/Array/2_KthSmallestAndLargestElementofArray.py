aa801c952f207a35aaba186208be438f4be0af00
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2023-05-26 17:59:50 +0545

    Update 2_KthSmallestAndLargestElementofArray.py

--- 450dsa/Array/2_KthSmallestAndLargestElementofArray.py
+++ 450dsa/Array/2_KthSmallestAndLargestElementofArray.py
@@ -1,5 +1,14 @@
 '''
 https://www.codingninjas.com/codestudio/problems/kth-smallest-and-largest-element-of-array_1115488?topList=love-babbar-dsa-sheet-problems&utm_source=website&utm_medium=affiliate&utm_campaign=450dsatracker&leftPanelTab=0
+
+Two Cases:
+    i)  Has Distinct Element
+        Approach 1: Create a set(sorted) and iterate over to the nth smallest and largest element 
+        Approach 2: Use min-heap, max-heap, priority-queue and iterate till the nth smallest and largest
+    ii) Has Duplicates
+        Approach 1: Sort the array
+                    Return the kth smallest and largest index
+
 '''
 
 def kthSmallLarge(arr, n, k):
