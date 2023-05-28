e2a69b0f27699b5322312106b97abe1977f32f3c
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2023-05-28 23:18:06 +0545

    Update 1_SecondLargest.py

--- strivers/3_1_ArraysEasy/1_SecondLargest.py
+++ strivers/3_1_ArraysEasy/1_SecondLargest.py
@@ -3,12 +3,33 @@ https://practice.geeksforgeeks.org/problems/second-largest3735/1?utm_source=yout
 '''
 
 # Naive : Sort and return second largest
+# This works only when there are no duplicates.
 # O(nlogn)
 class Solution:
 
 	def print2largest(self,arr, n):
 		return sorted(arr)[-2]
-  
+
+# Approach: 
+# In first pass find the largest element
+# In second pass find the second largest element
+import heapq
+
+class Solution:
+
+	def print2largest(self,arr, n):
+        largest = max(arr)
+        
+	# FIRST PASS
+        second_largest = float("-inf")
+        
+	# SECOND PASS
+        for item in arr:
+            if item > second_largest and item != largest:
+                second_largest = item
+                
+        return -1 if second_largest == float("-inf") else second_largest
+	
 # Using max-heap
 import heapq
 
