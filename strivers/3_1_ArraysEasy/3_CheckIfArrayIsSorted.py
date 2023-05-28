ea89bd817b4b30a8d99ccb3ee06d0740d41bbdf3
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2023-05-29 00:19:31 +0545

    Update 3_CheckIfArrayIsSorted.py

--- strivers/3_1_ArraysEasy/3_CheckIfArrayIsSorted.py
+++ strivers/3_1_ArraysEasy/3_CheckIfArrayIsSorted.py
@@ -15,3 +15,26 @@ class Solution:
                 if arr[idx+1] < arr[idx]:
                     return 0
         return 1
+    
+    
+'''
+https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/submissions/959149912/
+'''
+class Solution:
+
+    def check(self, nums: List[int]) -> bool:
+        # Find rotation index and num_of_rotations = 0
+        # if num_of_rotations is more than 1 then it is not sorted rotated
+        num_of_rotations = 0
+        for idx, item in enumerate(nums):
+            if idx+1 < len(nums):
+                if nums[idx+1] < nums[idx]:
+                    num_of_rotations += 1
+                    
+        if num_of_rotations > 1:
+            return False
+        # Corner Case, number of rotations is just 1 i.e first element will never be less than the last element
+        elif num_of_rotations == 1 and nums[0] < nums[len(nums)-1]:
+            return False
+        else:
+            return True
