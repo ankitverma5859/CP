abd7412d0eff3925d98ac574bf5c66ffb2e0c832
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2023-06-04 16:01:07 +0545

    Update 7_PeakElement.py

--- strivers/4_1_bs_easy/7_PeakElement.py
+++ strivers/4_1_bs_easy/7_PeakElement.py
@@ -58,3 +58,18 @@ class Solution:
                 l = mid + 1
 
         return l # or start index
+    
+    
+# Peak Element love babbar logic
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid] < nums[mid+1]:
+                l = mid + 1
+            else:
+                r = mid # watchout
+        return l
