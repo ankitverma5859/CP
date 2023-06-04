63513a17d00ee624e80627d504f3024740ca72e0
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2023-06-04 12:53:03 +0545

    Update 7_PeakElement.py

--- strivers/4_1_bs_easy/7_PeakElement.py
+++ strivers/4_1_bs_easy/7_PeakElement.py
@@ -25,3 +25,36 @@ class Solution:
                 peak_element_idx = len(nums)-1
 
         return peak_element_idx
+    
+# Time Complexity: O(logn)
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        nums_len = len(nums)
+        
+        # If the list as only one element
+        if nums_len == 1:
+            return 0
+
+        # Binary Search: following the larger element
+        l = 0
+        r = nums_len - 1
+        while l < r:
+            mid = (l+r)//2
+            
+            # Check if first element is the peak element
+            if mid == 0:
+                return 0 if nums[0] > nums[1] else 1
+
+            # Check if last element is the peak element
+            if mid == nums_len - 1:
+                return  nums_len-1 if nums[nums_len-1] > nums[nums_len - 2] else nums_len - 2
+
+            if nums[mid] >= nums[mid-1] and nums[mid] >= nums[mid+1]:
+                return mid
+            
+            if nums[mid] < nums[mid-1]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l # or start index
